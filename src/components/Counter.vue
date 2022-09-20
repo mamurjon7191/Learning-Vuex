432f70e9d2f16722553a8b37a6a86fd8902f5d69
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-09-20 09:59:14 +0500

    mutations has finished

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,16 +1,46 @@
 <template>
   <div>
-    <h1>Vuex Counter</h1>
-    <h1 class="counter">10</h1>
+    <h1>{{ counter }}</h1>
+    <button v-on:click="addCounter(value)">+</button>
+    <input type="number" v-model="value" />
+    <button v-on:click="addCounter(value)">+</button>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+      value: 1,
+    };
+  },
+  computed: {
+    ...mapState(["counter"]),
+  },
+  methods: {
+    ...mapMutations(["addCounter", "removeCounter"]),
+  },
+};
 </script>
 
 <style>
 .counter {
   font-size: 2rem;
+}
+button {
+  width: 2rem;
+  height: 2rem;
+  border-radius: 100%;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+input {
+  text-align: center;
+  margin: 0 0.5rem;
+  padding: 0.4rem;
+  font-size: 1rem;
+  outline: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      counter: 200,
+    };
+  },
+  mutations: {
+    addCounter(state, payload) {
+      state.counter = state.counter + payload;
+    },
+    removeCounter(state, payload) {
+      state.counter = state.counter - payload;
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.use(store);
+app.mount("#app");
